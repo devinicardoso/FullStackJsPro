033a24a72db1b9b92f7a87d5185ad6095f94889d
Vinicius Cardoso <75495504+vinicxxl@users.noreply.github.com>
2021-12-06 11:40:10 -0300

    Módulo JavaScript - Semana 3

---
+++ Módulo JavaScript - Terceira Semana/ex1/ex1.js
@@ -0,0 +1,7 @@
+alert("Bem-vindo! A Seguir pediremos que informe alguns dados.")
+
+let name = prompt("Informe o seu nome:")
+let age = prompt("Informe a sua idade:")
+let ageConfirm = confirm("Confirme sua idade: " + age + " Anos")
+
+alert("Nome: " + name + "\nidade: " + age + " Anos" + "\nConfirmação de idade: " + ageConfirm) 
---
+++ Módulo JavaScript - Terceira Semana/ex2/main.js
@@ -0,0 +1,17 @@
+// Pessoa mais velha
+let nomeMaior = prompt ("Informe o nome da pessoa mais velha:")
+let idadeMaior = prompt ("Informe a idade da pessoa mais velha:")
+
+// Pessoa mais nova
+let nomeMenor = prompt ("Informe o nome da pessoa mais nova:")
+let idadeMenor= prompt ("Informe a idade da pessoa mais nova:")
+
+// Calculo de diferaça de idade
+let diferenca = idadeMaior - idadeMenor
+
+alert(
+    "Pessoa mais velha:\nNome: " + nomeMaior + "\nIdade: " + idadeMaior + 
+    "\n\nPessoa mais nova:\nNome: " + nomeMenor + "\nIdade: " + idadeMenor + 
+    "\n\nDiferença de idade: " + diferenca + " Anos"
+)
+
