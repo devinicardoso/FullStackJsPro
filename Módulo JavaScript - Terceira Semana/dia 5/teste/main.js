e9e9eccd6f8bb90959ec05458af19811714542eb
Vinicius Cardoso <75495504+vinicxxl@users.noreply.github.com>
2022-01-10 10:20:57 -0300

    dia 5 - Array JS

---
+++ Módulo JavaScript - Terceira Semana/dia 5/teste/main.js
@@ -0,0 +1,18 @@
+let hitchedSpaceship = ["Colossus", "Elemental", "Helmet"]
+let guardar = ""
+
+console.log(hitchedSpaceship)
+
+guardar = hitchedSpaceship[0]
+
+let removedSpaceship = hitchedSpaceship.shift()
+
+console.log(hitchedSpaceship)
+
+console.log(removedSpaceship)
+
+let AddSpaceship = hitchedSpaceship.push(guardar)
+
+console.log(hitchedSpaceship)
+
+console.log(AddSpaceship)
